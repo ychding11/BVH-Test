a526834175f456643430701116edebdda95a8447
Yaochuang <ych_ding@163.com>
2019-06-19 08:49:18 +0800

    add new file.

---
+++ visualize/Primitives.h
@@ -0,0 +1,12 @@
+#ifndef PRIMITIVES_H
+#define PRIMITIVES_H
+
+#include "smallpt.h"
+
+namespace smallpt
+{
+
+
+}
+
+#endif
---
+++ visualize/Vector3.h
@@ -0,0 +1,12 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+#include "smallpt.h"
+
+namespace smallpt
+{
+
+
+}
+
+#endif
